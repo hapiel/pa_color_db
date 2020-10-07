af88f27b60e6c0609702542a06dc729a93c8615c
Marinus <marinusvandenoever@gmail.com>
2020-10-07 16:17:27 +0200

    fixed baseurl

--- Scraper.py
+++ Scraper.py
@@ -1,13 +1,16 @@
 import bs4
 import requests
 
-base_url = 'http://pixeljoint.com/pixelart/'
-generated_urls = [f"{base_url}{pnum}.htm" for pnum in range(1000, 1005)]
+base_url = 'http://pixeljoint.com'
+generated_urls = [f"{base_url}/pixelart/{pnum}.htm" for pnum in range(1000, 1005)]
 
 for url in generated_urls:
     response = requests.get(url)
     soup = bs4.BeautifulSoup(response.text, "html.parser")
     image = soup.find(id="mainimg")
     if image:
-        print(image['src'])
+        print('title '+image['alt'])
+        print('width '+image['width'])
+        print('height '+image['height'])
+        print('src '+image['src'])
     
